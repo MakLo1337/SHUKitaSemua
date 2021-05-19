a50e07bbf7f7ffc6dfe94a3b85f1efbca98a5741
Achmad Rijalu <achmad.rijalu@gmail.com>
2021-05-19 22:16:13 +0700

    Design commit

---
+++ app/src/main/java/com/example/shu_kita_semua/edit_koperasi.java
@@ -0,0 +1,15 @@
+package com.example.shu_kita_semua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class edit_koperasi extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_koperasi);
+        getSupportActionBar().hide();
+    }
+}
