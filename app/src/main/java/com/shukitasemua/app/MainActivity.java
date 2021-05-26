ea31ccab2a34f43da872c7274e58a8f263d90573
Achmad Rijalu <achmad.rijalu@gmail.com>
2021-05-26 20:42:14 +0700

    Fragment Added

--- app/src/main/java/com/shukitasemua/app/MainActivity.java
+++ app/src/main/java/com/shukitasemua/app/MainActivity.java
@@ -1,15 +1,63 @@
 package com.shukitasemua.app;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().hide();
+
+        BottomNavigationView btnNav = findViewById(R.id.bottomNavigationview);
+        btnNav.setOnNavigationItemSelectedListener(navlistener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navlistener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()){
+                        case R.id.item1:
+                            selectedFragment = new HomeFragment();
+                            break;
+
+                        case R.id.item2:
+                            selectedFragment = new add_member();
+                            break;
+                        case R.id.item3:
+                            selectedFragment = new halaman_shu();
+                    }
+
+                    //begin
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+
+//    private void replaceFragement(Fragment fragment) {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+//        fragmentTransaction.replace(R.id.frameLayout, fragment);
+//        fragmentTransaction.commit();
+//    }
 }
